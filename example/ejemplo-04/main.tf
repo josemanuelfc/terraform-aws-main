98281df47caaa54e4edec7597c27ac92445c7e31
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-18 20:57:02 +0100

    Add files via upload

---
+++ example/ejemplo-04/main.tf
@@ -0,0 +1,64 @@
+# Configuramos el proveedor de AWS
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Creamos un grupo de seguridad
+resource "aws_security_group" "sg_ejemplo_04" {
+  name        = "sg_ejemplo_04"
+  description = "Grupo de seguridad para la instancia de ejemplo 04"
+
+  # Reglas de entrada para permitir el tráfico SSH, HTTP y HTTPS
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Reglas de salida para permitir todas las conexiones salientes
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Creamos una instancia EC2
+resource "aws_instance" "instancia_ejemplo_04" {
+  ami             = "ami-00874d747dde814fa"
+  instance_type   = "t2.small"
+  key_name        = "vockey"
+  security_groups = [aws_security_group.sg_ejemplo_04.name]
+
+  user_data = file("scripts/install_docker.sh")
+
+  tags = {
+    Name = "instancia_ejemplo_04"
+  }
+}
+
+# Creamos una IP elástica y la asociamos a la instancia
+resource "aws_eip" "ip_elastica" {
+  instance = aws_instance.instancia_ejemplo_04.id
+}
+
+# Mostramos la IP pública de la instancia
+output "elastic_ip" {
+  value = aws_eip.ip_elastica.public_ip
+}
