fc9acf29504e7ca80caff02d53d2edfad2392394
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 20:20:01 +0100

    Update variables.tf

--- example/ejemplo-07/modules/security/variables.tf
+++ example/ejemplo-07/modules/security/variables.tf
@@ -1,1 +1,13 @@
+variable "name" {}
+variable "description" {}
+variable "vpc_id" {}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
 
