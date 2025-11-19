4269ef100bc4a024917a962dfe54e759e8f08f83
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 21:29:19 +0100

    Update main.tf

--- example/ejemplo-07/modules/compute/main.tf
+++ example/ejemplo-07/modules/compute/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "this" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.sg_ids
   key_name               = var.key_name
+  associate_public_ip_address = var.associate_public_ip_address
 
   user_data = var.user_data
 
