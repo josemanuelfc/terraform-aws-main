efb7e5fd085cc8fae3b59d324cc9adb4765d0b55
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 20:18:01 +0100

    Update main.tf

--- example/ejemplo-07/modules/subnets/main.tf
+++ example/ejemplo-07/modules/subnets/main.tf
@@ -1,1 +1,11 @@
+resource "aws_subnet" "this" {
+  count             = length(var.subnet_cidrs)
+  vpc_id            = var.vpc_id
+  cidr_block        = var.subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "${var.name}-${count.index}"
+  }
+}
 
