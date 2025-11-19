6b3f939998d56703bab2dc083dfad9f0de7f2d33
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 20:22:02 +0100

    Update outputs.tf

--- example/ejemplo-07/modules/outputs.tf
+++ example/ejemplo-07/modules/outputs.tf
@@ -1,1 +1,16 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "subnet_ids" {
+  value = module.subnets.subnet_ids
+}
+
+output "sg_id" {
+  value = module.security.sg_id
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
 
