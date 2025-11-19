ae299ef9a73286a0a3ebbbbd8d23921cc916c24d
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 22:08:03 +0100

    Update main.tf

--- example/ejemplo-07/modules/vpc/main.tf
+++ example/ejemplo-07/modules/vpc/main.tf
@@ -1,3 +1,7 @@
+variable "cidr_block" {}
+variable "name" {}
+
+# 1. Crear VPC
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
@@ -7,4 +11,30 @@ resource "aws_vpc" "this" {
     Name = var.name
   }
 }
+
+# 2. Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+# 3. Route Table pública
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-public-rt"
+  }
+}
+
+# 4. Ruta hacia Internet
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
 
