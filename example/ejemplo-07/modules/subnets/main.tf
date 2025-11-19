a6388e18b2845501dbc7ef48a1e2fc43a89ddb19
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 22:09:23 +0100

    Update main.tf

--- example/ejemplo-07/modules/subnets/main.tf
+++ example/ejemplo-07/modules/subnets/main.tf
@@ -3,9 +3,15 @@ resource "aws_subnet" "this" {
   vpc_id            = var.vpc_id
   cidr_block        = var.subnet_cidrs[count.index]
   availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true   # ← ESTO HACE LA SUBNET PÚBLICA
+
 
   tags = {
     Name = "${var.name}-${count.index}"
   }
 }
-
+resource "aws_route_table_association" "public" {
+  count          = length(var.subnet_cidrs)
+  subnet_id      = aws_subnet.this[count.index].id
+  route_table_id = var.public_route_table_id
+}
