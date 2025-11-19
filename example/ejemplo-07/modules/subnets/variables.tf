daaedd75210de01387bace87373a71dfcbaa50df
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 20:18:28 +0100

    Update variables.tf

--- example/ejemplo-07/modules/subnets/variables.tf
+++ example/ejemplo-07/modules/subnets/variables.tf
@@ -1,1 +1,20 @@
+variable "vpc_id" {
+  type        = string
+  description = "ID de la VPC"
+}
+
+variable "subnet_cidrs" {
+  type        = list(string)
+  description = "Lista de CIDRs para las subnets"
+}
+
+variable "azs" {
+  type        = list(string)
+  description = "Zonas de disponibilidad"
+}
+
+variable "name" {
+  type        = string
+  description = "Prefijo de nombre para las subnets"
+}
 
