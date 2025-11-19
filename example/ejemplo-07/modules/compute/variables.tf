8d4b5c5bfff7e9bb19d0641066caf6b05ecc92cd
josemanuelfc <josemanuelfc@josemanuelfc.es>
2025-11-19 20:21:01 +0100

    Update variables.tf

--- example/ejemplo-07/modules/compute/variables.tf
+++ example/ejemplo-07/modules/compute/variables.tf
@@ -1,1 +1,12 @@
 
+variable "ami" {}
+variable "instance_type" {}
+variable "subnet_id" {}
+variable "sg_ids" {
+  type = list(string)
+}
+variable "key_name" {}
+variable "name" {}
+variable "user_data" {
+  default = null
+}
